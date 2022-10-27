0101b2ed867ecfcfc3e9c2ea2d0c0879412ccde7
Deeksha <deeksha.tw@apollo.io>
2022-10-27 14:18:44 +0530

    If opponent ship is in target position, player hit the ship

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,8 @@ require_relative 'ship'
 class Player
 
   ALIVE = 'B'
+  HIT = 'X'
+  MISS = 'O'
   def create_grid(grid_size)
     @grid ||= Array.new(grid_size) { Array.new(grid_size) { '-' } }
   end
@@ -20,5 +22,31 @@ class Player
     end
   end
 
+  def target(positions)
+    @hit=0
+    positions.each do |g|
+      if ship_found_in_target_position(g[0].to_i,g[1].to_i)
+        hit_the_ship(g[0].to_i,g[1].to_i)
+      else
+        miss_the_ship(g[0].to_i,g[1].to_i)
+      end
+    end
+    [@grid, @hit]
+  end
+
+  private
+
+  def ship_found_in_target_position(x,y)
+    @grid[x][y] == ALIVE
+  end
+
+  def hit_the_ship(x,y)
+    @grid[x][y] = HIT
+    @hit += 1
+  end
+
+  def miss_the_ship(x,y)
+    @grid[x][y] = MISS
+  end
 end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,4 +19,18 @@ describe Player do
 
     expect(grid[x_axis][y_axis]).to eq Player::ALIVE
   end
+
+  it 'should HIT ship if ship is in target position' do
+    player = Player.new
+    grid = player.create_grid(5)
+    x_axis = 0
+    y_axis = 1
+    target_x_axis = 0
+    target_y_axis = 1
+    player.place_ships_in_grid([[x_axis, y_axis]])
+
+    player.target([[target_x_axis,target_y_axis]])
+
+    expect(grid[target_x_axis][target_y_axis]).to eq Player::HIT
+  end
 end
