46412be528a02b617c2c75c8aea78596834ef385
Deeksha <deeksha.tw@apollo.io>
2022-10-27 14:09:56 +0530

    Support file read on battleship to fetch data

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,9 +1,14 @@
+require_relative 'FileReaderWriter'
 class Battleship
 
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :file
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+
+    @file = FileReaderWriter.new
   end
+
+
 end
--- spec/battleship_spec.rb
+++ spec/battleship_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/battleship'
+require_relative '../lib/fileReaderWriter'
 
 describe Battleship do
   let(:player1) { double("Player") }
@@ -10,5 +11,11 @@ describe Battleship do
     expect(bat.player1).not_to be_nil
     expect(bat.player2).not_to be_nil
   end
-  
+
+  it 'should create file reader instance to read data' do
+    bat = Battleship.new(player1, player2)
+
+    expect(bat.file).to be_a FileReaderWriter
+  end
+
 end
